b6cc1a2612b7985d5c1272ab2d4b1acd201c75ef
brian <trmthanh220895@gmail.com>
2023-09-10 20:49:18 +0700

    Thanh 2

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
 <>
 <input type='text' placeholder='Name..' onChange={(event)=>setNewName(event.target.value)}/>
 <input type='number' placeholder='Age..' onChange={(event)=>setNewAge(event.target.value)}/>
-<button onClick={createUser}>Create user</button>
+<button onClick={createUser}>Create users</button>
 {users.map((user)=>{
   return (
     <div>
